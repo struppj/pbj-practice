7aafa804b79686b9e10c71d070cf372f6e8665c5
struppj <strupp.j@gmail.com>
2014-02-02 11:37:52 -0500

    Create pbj.py

---
+++ pbj.py
@@ -0,0 +1,73 @@
+#goal 1 
+
+peanut_butter = 1 
+
+jelly = 1 
+
+bread = 7 
+
+if peanut_butter == 1 and jelly == 1 and bread >=2:
+	print "I can make exactly one sandwich"
+	
+if peanut_butter < 1 or jelly < 1 or bread <=1:
+	print "No sandwich for me"	 
+
+#goal 2 
+
+peanut_butter = 4 
+
+jelly = 6 
+
+bread = 9 
+
+if bread >= 2 and peanut_butter >= 1 and jelly >= 1:
+	print "I can make exactly one sandwich!"
+	
+if bread < 2 and peanut_butter < 1 and jelly < 1: 
+	print "No sandwich for me" 
+	
+#goal 3 
+
+peanut_butter = 5 
+
+jelly = 5 
+
+bread = 9 
+
+if bread % 2 == 1 and peanut_butter % 2 == 1 and jelly % 2 == 1: 
+	print "You have an open faced sandwich" 
+
+#goal 4 
+
+peanut_butter = 3 
+
+jelly = 3 
+
+bread = 5 
+
+if bread % 2 == 1 and peanut_butter % 2 == 1 and jelly % 2 == 1:
+	print "You have an open faced sandwich"
+	
+if bread % 2 == 1 and peanut_butter % 2 == 1 and jelly % 2 == 0:
+	print "You are missing jelly"
+	
+if bread % 2 == 0 and peanut_butter % 2 == 1 and jelly % 2 == 1:
+	print "You are missing bread"
+	
+if bread % 2 == 1 and peanut_butter % 2 == 0 and jelly % 2 == 1:
+	print "You are missing peanut butter"  
+
+#goal 5 
+
+peanut_butter = 5 
+
+jelly = 5 
+
+bread = 9 
+
+if bread >= 2 and peanut_butter is >= 1 and jelly is >=1:
+	print "You can make a peanut butter and jelly sandwich!"
+
+if bread >= 2 and peanut_butter is >= 1 and jelly is <1:
+	print "You can make a peanut butter sandwich"
+	
